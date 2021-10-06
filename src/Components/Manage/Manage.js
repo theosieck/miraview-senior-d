f147eb5275a129a39622b0e44304d515765c8480
Noah Suttora <njsuttora@optimum.net>
2021-10-06 17:10:12 -0400

    patient list component rough draft

--- src/Components/Manage/Manage.js
+++ src/Components/Manage/Manage.js
@@ -1,6 +1,33 @@
+import {useState} from 'react';
 
 function Manage () {
-	return <h1>Manage Page</h1>;
+	return (
+		<div>
+			<h1>Manage Page</h1>
+			<PatientList />
+		</div>
+	);
+}
+
+/*
+TODO: formatting: boxes around names, left-aligned & vertically centered, spacing btwn names
+TODO: onClick functionality for each patient name for client profile component
+TODO: new patient button functionality with patient name input
+TODO: hide bullet points (use <li> or something else?)
+*/
+function PatientList() {
+	const names = ['Nicholas Gattuso', 'Essence Peters', 'Derek Morris', 'Alex Conetta', 'Gene Donovan', 'Alex Stupar', 'Nicholas Gattuso'];
+	const [patients, addPatient] = useState(names);
+	const listPatients = patients.map((name) => <li>{name}</li>)
+
+	return (
+		<div>
+			<b><p>{listPatients}</p></b>
+			<button onClick={() => addPatient(patients => [...patients, 'name'])}>
+				+ New Patient
+			</button>
+		</div>
+	);
 }
 
 export default Manage;
