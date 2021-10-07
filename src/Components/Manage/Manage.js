98094e15cf1d9f2cb736a0f5716b3ccafc33605e
Noah Suttora <njsuttora@optimum.net>
2021-10-07 15:01:18 -0400

    patient name input & rough list onClick functionality

--- src/Components/Manage/Manage.js
+++ src/Components/Manage/Manage.js
@@ -10,20 +10,48 @@ function Manage () {
 }
 
 /*
-TODO: formatting: boxes around names, left-aligned & vertically centered, spacing btwn names
+TODO: formatting: boxes around names, left-aligned & vertically centered, spacing btwn names, column list on left with profile to right of it
 TODO: onClick functionality for each patient name for client profile component
-TODO: new patient button functionality with patient name input
-TODO: hide bullet points (use <li> or something else?)
+TODO: hide bullet points (css)
+TODO: change input & button for adding a new patient to just button like design?
+TODO: more checks on patient name input
 */
 function PatientList() {
+	
+	function handlePatientListClick(listIndex) {
+		// temporary
+		document.title = listIndex.toString();
+	}
+
+	function handleAddPatientClick(name) {
+		if (name === '') {
+			//setInputValue('');
+			return;
+		}
+		addPatient(patients => [...patients, name]);
+		setInputName('');
+	}
+
 	const names = ['Nicholas Gattuso', 'Essence Peters', 'Derek Morris', 'Alex Conetta', 'Gene Donovan', 'Alex Stupar', 'Nicholas Gattuso'];
+	
+	// state hooks
 	const [patients, addPatient] = useState(names);
-	const listPatients = patients.map((name) => <li>{name}</li>)
+	const [inputName, setInputName] = useState('');
+	
+	const listPatients = patients.map((name, index) => 
+		<ul>
+			<li key={index} onClick={() => handlePatientListClick(index)}>
+				{name}
+			</li>
+		</ul>
+	);
 
 	return (
 		<div>
 			<b><p>{listPatients}</p></b>
-			<button onClick={() => addPatient(patients => [...patients, 'name'])}>
+			
+			<input value={inputName} onChange={(e) => setInputName(e.target.value)} placeholder='Patient Name...' />
+			<button onClick={() => handleAddPatientClick(inputName)}>
 				+ New Patient
 			</button>
 		</div>
