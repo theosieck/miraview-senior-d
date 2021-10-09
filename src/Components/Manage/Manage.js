51818dfe42dbda564e261ecaece4207272d4a3be
Noah Suttora <njsuttora@optimum.net>
2021-10-09 16:18:59 -0400

    formatting & input 'enter' event

--- src/Components/Manage/Manage.js
+++ src/Components/Manage/Manage.js
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import './Manage.css';
 // possibly remove some of these imports
 import {styled} from '@mui/material/styles';
 import Box from '@mui/material/Box';
@@ -24,14 +25,34 @@ const Item = styled(Paper)(({ theme }) => ({
   
 
 /*
-TODO: formatting: boxes around names, left-aligned & vertically centered, spacing btwn names, hide bullet points (css)
+TODO: formatting like design
 TODO: change input & button for adding a new patient to just button like design?
-TODO: hitting enter in input presses button
+TODO: keep <ul>,<li> or switch to material ui's list, listitem, listitemtext?
 */
 function PatientList() {
+	const names = ['Nicholas Gattuso', 'Essence Peters', 'Derek Morris', 'Alex Conetta', 'Gene Donovan', 'Alex Stupar', 'Nicholas Gattuso'];
+	
+	// state hooks
+	const [patients, addPatient] = useState(names);
+	const [inputName, setInputName] = useState('');
+	const [patientFocused, showPatientProfile] = useState('');
+
+	const listPatients = patients.map((name, index) => 
+		<ul class='patientList'>
+			<li key={index} onClick={() => handlePatientListClick(index)} class='patientListBorder'>
+				{name}
+			</li>
+		</ul>
+	);
 	
 	function handlePatientListClick(index) {
 		showPatientProfile(patients[index] + ' profile here');
+	}
+
+	function handleAddPatientEnterPress(e, inputName) {
+		if (e.key === 'Enter') {
+			handleAddPatientClick(inputName);
+		}
 	}
 
 	function handleAddPatientClick(name) {
@@ -48,30 +69,15 @@ function PatientList() {
 		setInputName('');
 	}
 
-	const names = ['Nicholas Gattuso', 'Essence Peters', 'Derek Morris', 'Alex Conetta', 'Gene Donovan', 'Alex Stupar', 'Nicholas Gattuso'];
-	
-	// state hooks
-	const [patients, addPatient] = useState(names);
-	const [inputName, setInputName] = useState('');
-	const [patientFocused, showPatientProfile] = useState('');
-	
-	const listPatients = patients.map((name, index) => 
-		<ul>
-			<li key={index} onClick={() => handlePatientListClick(index)}>
-				{name}
-			</li>
-		</ul>
-	);
-
 	return (
 		<div>
 			<Box>
 				<Grid container spacing={1}>
 					<Grid item xs={3}>
 						<Item>Column 1</Item>
-						<b><p>{listPatients}</p></b>
-			
-						<input value={inputName} onChange={(e) => setInputName(e.target.value)} placeholder='Patient Name...' />
+						<h3>{listPatients}</h3>
+						<input 	value={inputName} onChange={(e) => setInputName(e.target.value)}
+								onKeyDown={(e) => handleAddPatientEnterPress(e, inputName)} placeholder='Patient Name...' />
 						<button onClick={() => handleAddPatientClick(inputName)}>
 							+ New Patient
 						</button>
