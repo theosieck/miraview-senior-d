42f25918f96eef933409516957bcc5e898d0c556
Cassidy Savettiere <cassiesave@gmail.com>
2021-10-12 11:59:41 -0400

    style changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, makeStyles } from '@material-ui/core';
+import { Box, Grid, makeStyles } from '@material-ui/core';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PeopleIcon from '@mui/icons-material/People';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
@@ -11,23 +11,27 @@ function Home () {
 }
 
 return(
-    <Grid container spacing={3}>
-        <Grid item xs={6} sm={3}>
-            <Paper className={classes.paper}>My Clients
-            </Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-            <PlayArrowIcon fontSize="small"></PlayArrowIcon>
-            <Paper className={classes.paper}>3 Active Today</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-            <PeopleIcon fontSize="small"></PeopleIcon>
-            <Paper className={classes.paper}>13 Active Clients</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-            <ArrowUpwardIcon fontSize="small"></ArrowUpwardIcon>
-            <Paper className={classes.paper}>+10% Grounding Activations</Paper>
-        </Grid>
-    </Grid>
+	<div>
+		<Box>
+			<Grid container spacing={3}>
+				<Grid item xs={6} sm={3}>
+					<Paper className={classes.paper}>My Clients
+					</Paper>
+				</Grid>
+				<Grid item xs={6} sm={3}>
+					<PlayArrowIcon fontSize="small"></PlayArrowIcon>
+					<Paper className={classes.paper}>3 Active Today</Paper>
+				</Grid>
+				<Grid item xs={6} sm={3}>
+					<PeopleIcon fontSize="small"></PeopleIcon>
+					<Paper className={classes.paper}>13 Active Clients</Paper>
+				</Grid>
+				<Grid item xs={6} sm={3}>
+					<ArrowUpwardIcon fontSize="small"></ArrowUpwardIcon>
+					<Paper className={classes.paper}>+10% Grounding Activations</Paper>
+				</Grid>
+			</Grid>
+		</Box>
+	</div>   
 )
 export default Home;
