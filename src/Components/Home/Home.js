657a02183efa9db73bfa370044a70cc4d03ac2da
coriwisniewski <43626560+coriwisniewski@users.noreply.github.com>
2021-10-16 16:14:23 -0400

    added client summary grid
    adjusted some spacing

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, Grid, Paper } from '@material-ui/core';
+import { Box, Grid, Paper, Avatar } from '@material-ui/core';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PeopleIcon from '@mui/icons-material/People';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
@@ -54,49 +54,59 @@ function ClientOverview()
 	);
 }
 
-function ClientGrid()
+function ClientRow(props)
 {
-	const names = ['Nicholas Gattuso', 'Essence Peters', 'Derek Morris', 'Alex Conetta', 'Gene Donovan', 'Alex Stupar', 'Nicholas Gattuso'];
-
-	const clientList = names.map((name, index) =>
-		<Grid container spacing={0} xs={8}>
+	const {
+		name
+	} = props;
+	return (
+		<Grid container spacing={0} xs={11}>
 			<Grid item xs={3}>
-				<h3>{name}</h3>
-				<h4>Age: 42</h4>
+				<h3 className="name">{name}</h3>
+				<h4 className="age">Age: 42</h4>
 			</Grid>
 			<Grid item xs={3}>
 				<Grid container spacing={0}>
-					<Grid item xs={4}>
-						<TrendingDownIcon fontSize="small"></TrendingDownIcon>
+					<Grid className="verticalCenter" item xs={2}>
+						<TrendingDownIcon fontSize="large"></TrendingDownIcon>
 					</Grid>
 					<Grid item xs={4}>
-						<h3>-5%</h3>
+						<h3 className="score">-5%</h3>
 					</Grid>
-					<Grid item xs={4}>
-						<h4>Symptom</h4>
-						<h4>Scores</h4>
+					<Grid item xs={6}>
+						<h4>Symptom Scores</h4>
 					</Grid>
 				</Grid>
 				
 			</Grid>
 			<Grid item xs={3}>
 				<Grid container spacing={0}>
-					<Grid item xs={4}>
-						<TrendingUpIcon fontSize="small"></TrendingUpIcon>
+					<Grid className="verticalCenter" item xs={2}>
+						<TrendingUpIcon fontSize="large"></TrendingUpIcon>
 					</Grid>
 					<Grid item xs={4}>
-						<h3>+10%</h3>
+						<h3 className="score">+10%</h3>
 					</Grid>
-					<Grid item xs={4}>
-						<h4>Grounding</h4>
-						<h4>Activations</h4>
+					<Grid item xs={6}>
+						<h4 style={{marginLeft: '15px'}}>Grounding Activations</h4>
 					</Grid>
 				</Grid>
 			</Grid>
 			<Grid item xs={3}>
 				<Button>View Patient Data<ArrowRightAlt></ArrowRightAlt></Button>
 			</Grid>
-		</Grid>);
+		</Grid>
+	);
+}
+
+function ClientGrid()
+{
+	const names = ['Nicholas Gattuso', 'Essence Peters', 'Derek Morris', 'Alex Conetta', 'Gene Donovan', 'Alex Stupar', 'Nicholas Gattuso'];
+	const clientList = names.map((name, index) =>
+		<div className="individualRow">
+			<ClientRow name={name}></ClientRow>
+		</div>
+		);
 
 	return(
 		<Box>
