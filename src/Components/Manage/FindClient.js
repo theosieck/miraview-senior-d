6d97636af8cbc882866871f458c5c7460c175d20
Jorie Sieck <jsieck@stevens.edu>
2021-11-19 13:50:14 -0500

    done with find client

--- src/Components/Manage/FindClient.js
+++ src/Components/Manage/FindClient.js
@@ -1,34 +1,47 @@
 import { useState } from "react";
 import { findClient, updateTherapist, auth } from "../../firebase/Firebase";
 import { Button, TextField, CircularProgress } from '@mui/material';
+import { useSelector, useDispatch } from "react-redux";
+import Alert from '@material-ui/lab/Alert';
 
 export default function FindClient () {
+	const userData = useSelector((state) => state.user);
+	const dispatch = useDispatch();
+	const clientListData = useSelector((state) => state.clientsList);
 	const [clientFound, setClientFound] = useState(null);
 	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
 
 	const searchForClient = async (e) => {
 		e.preventDefault();
 
-		// set loading
+		// set loading, error
 		setLoading(true);
+		setError(null);
 
 		// get the email we're looking for
 		let email = document.getElementById('client-email').value;
 		email = email.trim();
 
-		// TODO make sure client isn't already a client of this therapist
-
 		// look for the client
 		let result;
 		try {
 			result = await findClient({email}, auth);
-			if (result.data.code!==200) throw 'Error: something went wrong fetching the client';
+			if (result.data.code!==200) throw result.data;
 		} catch (e) {
-			console.log(e);
-			// TODO
+			if (e.message) setError(e.message);
+			else setError('Error: Something went wrong finding the client.');
+			setLoading(false);
+			return;
 		}
 
 		const client = result.data.data;
+		// make sure client isn't already a client of this therapist
+		if (userData.data.data.data.clients.includes(client.uid)) {
+			setError('This client is already in your client list.');
+			setLoading(false);
+			return;
+		}
 		setClientFound(client);
 
 		// clear the input
@@ -53,13 +66,21 @@ export default function FindClient () {
 		let result;
 		try {
 			result = await updateTherapist({clients:[clientID]}, auth);
-			if (result.data.code!==200) throw 'Error: something went wrong updating the therapist';
+			if (result.data.code!==200) throw result.data;
 		} catch (e) {
-			console.log(e);
-			//TODO
+			if (e.message) setError(e.message);
+			else setError('Error: Something went wrong finding the client.');
+			setLoading(false);
+			return;
 		}
 
-		// add client to redux store - TODO
+		// add client to redux store
+		clientListData.clients[clientID] = clientFound.name;
+		dispatch({
+			type: 'SET_CLIENT_DATA',
+			payload: clientListData
+		});
+		console.log(clientListData);
 
 		setLoading(false);
 	}
@@ -73,6 +94,8 @@ export default function FindClient () {
 			</form>
 
 			{loading && <CircularProgress id="find-client-loading" />}
+
+			{error && <Alert severity="error">{error}</Alert>}
 
 			{clientFound && <div id="client-found">
 				<p>Found client <span id="client-found-name">{clientFound.name}</span></p>
